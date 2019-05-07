22dccb2f5e100dcd2cfa315061deb8234a3a34e7
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-07 15:18:12 -0700

    Remove script from setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def run(packages, install_requires):
           packages=packages,
           install_requires=install_requires,
           package_dir={'': 'python'},
-          scripts=['bin/clu'],
+          scripts=[],
           classifiers=[
               'Development Status :: 4 - Beta',
               'Intended Audience :: Science/Research',
