b6fd80d0da66d5104d7b7ec08f9adeec95393c5b
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-18 20:30:07 -0700

    Move CommandStatus to top namespace

--- python/clu/__init__.py
+++ python/clu/__init__.py
@@ -7,12 +7,12 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-05-14 16:59:15
+# @Last modified time: 2019-05-18 20:29:50
 
 # flake8: noqa
 
 from .actor import *
-from .base import escape
+from .base import CommandStatus, escape
 from .command import *
 from .device import *
 from .exceptions import *
