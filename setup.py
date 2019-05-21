bc34c1a50532e385bc75478f0a53700b79db5455
José Sánchez-Gallego <gallegoj@uw.edu>
2019-05-20 20:29:34 -0700

    Simple but functional command line interpreter

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def run(packages, install_requires):
           packages=packages,
           install_requires=install_requires,
           package_dir={'': 'python'},
-          scripts=[],
+          scripts=['bin/clu'],
           classifiers=[
               'Development Status :: 4 - Beta',
               'Intended Audience :: Science/Research',
