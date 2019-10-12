ea36849af5a83785ac1d508e48aa84939c462e0a
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-11 17:51:19 -0700

    Add a placeholder test to make travis pass

---
+++ python/clu/tests/test_clu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2019-10-11
+# @Filename: test_clu.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+def test_placeholder():
+
+    assert True
