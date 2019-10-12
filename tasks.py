774d8584d5f2f6b10239c7106cad5cad19d5a6fa
José Sánchez-Gallego <gallegoj@uw.edu>
2019-10-11 17:40:51 -0700

    Update tasks

--- tasks.py
+++ tasks.py
@@ -58,17 +58,64 @@ def clean(ctx):
 
 
 @task(clean)
-def deploy(ctx):
+def deploy(ctx, test=False):
     """Deploy the project to pypi"""
-    print('Deploying to Pypi!')
-    ctx.run('python setup.py sdist bdist_wheel --universal')
-    ctx.run('twine upload dist/*')
+
+    if test is False:
+        print('Deploying to Pypi!')
+        repository_url = ''
+    else:
+        print('Deploying to Test PyPI!')
+        repository_url = '--repository-url https://test.pypi.org/legacy/'
+
+    ctx.run('python setup.py sdist bdist_wheel')
+    ctx.run(f'twine upload {repository_url} dist/*')
+
+
+@task(name='install-deps')
+def install_deps(ctx, extras=None):
+    """Install only dependencies from setup.cfg."""
+
+    import setuptools
+
+    if not os.path.exists('setup.cfg'):
+        raise RuntimeError('setup.cfg cannot be found. If your project uses '
+                           'requirement files use pip install -r instead.')
+
+    if extras:
+        extras = extras.split(',')
+    else:
+        extras = []
+
+    config = setuptools.config.read_configuration('setup.cfg')
+
+    if not config['options']:
+        return
+
+    options = config['options']
+
+    setup_requires = options.get('setup_requires', [])
+    install_requires = options.get('install_requires', [])
+
+    requires = setup_requires + install_requires
+    requires_str = (' '.join('"' + item + '"' for item in requires))
+    if len(requires) > 0:
+        ctx.run(f'pip install --upgrade {requires_str}', pty=True)
+
+    for extra in extras:
+        print(f'Installing extras={extra}')
+        if 'extras_require' not in options:
+            raise RuntimeError('extras_require is not defined')
+        extra_deps = options['extras_require'].get(extra, [])
+        if len(extra_deps) > 0:
+            extra_deps_str = (' '.join('"' + item + '"' for item in extra_deps))
+            ctx.run(f'pip install --upgrade {extra_deps_str}', pty=True)
 
 
 os.chdir(os.path.dirname(__file__))
 
 # create a collection of tasks
-ns = Collection(clean, deploy)
+ns = Collection(clean, deploy, install_deps)
 
 # create a sub-collection for the doc tasks
 docs = Collection('docs')
