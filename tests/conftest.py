cc35d6c17190b33c7e20e5303cf9728d30dd6f5d
José Sánchez-Gallego <gallegoj@uw.edu>
2020-08-26 19:57:20 -0700

    Set rabbitmq paths for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,17 @@
 # @Filename: conftest.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+import os
 from pytest_rabbitmq import factories
 
-rabbitmq_proc = factories.rabbitmq_proc(port=None)
+
+# If CI is true we are in a GitHub workflow and need to use the Ubuntu paths.
+if 'CI' in os.environ and os.environ['CI'] == 'true':
+    ctl = '/usr/sbin/rabbitmqctl'
+    server = '/usr/sbin/rabbitmq-server'
+else:
+    ctl = server = None
+
+
+rabbitmq_proc = factories.rabbitmq_proc(ctl=ctl, server=server, port=None)
 rabbitmq = factories.rabbitmq('rabbitmq_proc')
