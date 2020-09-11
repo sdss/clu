02c91a2483e4d41b8d9b0842be32a242b9848158
José Sánchez-Gallego <gallegoj@uw.edu>
2020-09-11 10:52:47 -0700

    Test for CluNotImplemented

---
+++ tests/test_exceptions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-09-11
+# @Filename: test_exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import pytest
+
+from clu.exceptions import CluNotImplemented
+
+
+def test_clu_not_implemented():
+
+    with pytest.raises(CluNotImplemented) as err:
+        raise CluNotImplemented()
+
+    assert str(err.value) == 'This feature is not implemented yet.'
