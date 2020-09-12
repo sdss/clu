1428f9023568491ef79bff12f3947d71f2be400a
José Sánchez-Gallego <gallegoj@uw.edu>
2020-09-12 10:53:32 -0700

    Move pytest-rabbitmq config to test.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,21 +20,7 @@ from clu.protocol import open_connection
 DATA_DIR = pathlib.Path(os.path.dirname(__file__)) / 'data'
 RMQ_PORT = 8888
 
-
-# If CI is true we are in a GitHub workflow and need to use the Ubuntu paths.
-# For now we are only testing Linux in CI so these are the paths where apt-get
-# installs rabbitmq-server on Ubuntu 20.04. If we add more architectures we'd
-# need to set some environment variable in the workflow YAML file with the
-# value of runner.os and then reference it here.
-# If ctl or server are None, the values are taken from pyproject.toml:pytest.
-if 'CI' in os.environ and os.environ['CI'] == 'true':
-    ctl = '/usr/lib/rabbitmq/bin/rabbitmqctl'
-    server = '/usr/lib/rabbitmq/bin/rabbitmq-server'
-else:
-    ctl = server = None
-
-
-rabbitmq_proc = factories.rabbitmq_proc(ctl=ctl, server=server, port=RMQ_PORT)
+rabbitmq_proc = factories.rabbitmq_proc(port=RMQ_PORT)
 rabbitmq = factories.rabbitmq('rabbitmq_proc')
 
 
