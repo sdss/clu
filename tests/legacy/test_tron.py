2817749c70b76368ae8cdac5ddaddc60fd3f163e
José Sánchez-Gallego <gallegoj@uw.edu>
2021-12-17 02:59:23 -0700

    Unused import

--- tests/legacy/test_tron.py
+++ tests/legacy/test_tron.py
@@ -15,7 +15,6 @@ import pytest
 from clu.legacy import TronConnection, TronKey
 from clu.legacy.types.messages import Reply
 from clu.legacy.types.parser import ParseError
-from clu.tools import CommandStatus
 
 
 pytestmark = [pytest.mark.asyncio]
